3740c962e1bfad0b0b980f537e216ec494d8028c
Antonov-S <anto1nov1@gmail.com>
2024-12-20 02:19:29 +0200

    Setup generatation of Jwt Token on login endpoint

--- app/api/login/route.ts
+++ app/api/login/route.ts
@@ -1,5 +1,8 @@
-import { sql } from "@/db";
 import { NextResponse } from "next/server";
+import bcrypt from "bcrypt";
+import { SignJWT } from "jose";
+
+import { sql } from "@/db";
 
 export async function POST(request: Request) {
   const json = await request.json();
@@ -7,6 +10,27 @@ export async function POST(request: Request) {
     "select id, username, password from users where username ilike $1",
     [json.username]
   );
+  if (res.rowCount === 0) {
+    return NextResponse.json({ error: "user not found" }, { status: 404 });
+  }
 
-  return NextResponse.json({ data: res.rows[0] });
+  const user = res.rows[0];
+  const match = await bcrypt.compare(json.password, user.password);
+  if (!match) {
+    return NextResponse.json({ error: "invalid credentials" }, { status: 401 });
+  }
+
+  const token = await new SignJWT({})
+    .setProtectedHeader({ alg: "HS256" })
+    .setSubject(user.id)
+    .setIssuedAt()
+    .setExpirationTime("2w")
+    .sign(new TextEncoder().encode(process.env.JWT_SECRET));
+  const response = NextResponse.json({ msg: "login success" });
+  response.cookies.set("jwt-token", token, {
+    sameSite: "strict",
+    httpOnly: true,
+    secure: true
+  });
+  return response;
 }
