0ed7cb453813dd74ddd96ec6717f4165a02e4aa7
Antonov-S <anto1nov1@gmail.com>
2024-12-23 23:28:46 +0200

    Fixing correct database url in production

--- db.ts
+++ db.ts
@@ -5,6 +5,21 @@ const projectDir = process.cwd();
 loadEnvConfig(projectDir);
 
 export async function getClient(): Promise<Client> {
+  console.log("Database client configuration:", {
+    POSTGRES_URL: process.env.DATABASE_URL,
+    POSTGRES_USER: process.env.POSTGRES_USER,
+    POSTGRES_HOST: process.env.POSTGRES_HOST,
+    POSTGRES_NAME: process.env.POSTGRES_NAME,
+    POSTGRES_PORT: process.env.POSTGRES_PORT
+  });
+
+  if (process.env.DATABASE_URL) {
+    const client = new Client({
+      connectionString: process.env.DATABASE_URL
+    });
+    return client;
+  }
+
   const client = new Client({
     user: process.env.POSTGRES_USER,
     host: process.env.POSTGRES_HOST,
@@ -12,7 +27,6 @@ export async function getClient(): Promise<Client> {
     password: process.env.POSTGRES_PASSWORD,
     port: parseInt(process.env.POSTGRES_PORT!)
   });
-
   return client;
 }
 
