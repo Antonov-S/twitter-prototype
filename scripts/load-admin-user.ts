e10a773444abb94ddf473c9e2dd940fb499f3242
Antonov-S <anto1nov1@gmail.com>
2025-01-03 04:09:01 +0200

    Updating the script that create admin user with avatar and try-cach block

--- scripts/load-admin-user.ts
+++ scripts/load-admin-user.ts
@@ -1,18 +1,31 @@
 import bcrypt from "bcrypt";
+import { faker } from "@faker-js/faker";
 
 import { getClient } from "@/db";
 
 async function loadAdminUser(username: string, password: string) {
-  console.log(`executing loading admin user ${username} pw ${password}`);
+  console.log(
+    `Executing loading admin user ${username} with the given password.`
+  );
   const saltRounds = 10;
   const hash = await bcrypt.hash(password, saltRounds);
+  const avatar = faker.image.avatar();
   const client = await getClient();
-  await client.connect();
-  await client.query(
-    "insert into public.users (username, password, is_admin) values ($1, $2, $3)",
-    [username, hash, true]
-  );
-  await client.end();
+
+  try {
+    await client.connect();
+    await client.query(
+      "insert into public.users (username, password, is_admin, avatar) values ($1, $2, $3, $4)",
+      [username, hash, true, avatar]
+    );
+    console.log(
+      `Admin user '${username}' created successfully with an avatar.`
+    );
+  } catch (error) {
+    console.error("Error while creating admin user:", error);
+  } finally {
+    await client.end();
+  }
 }
 
 const username = process.argv[2];
