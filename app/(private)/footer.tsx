795e5c183cd936a6a6dc9ec798cae047eddbb91b
Antonov-S <anto1nov1@gmail.com>
2025-01-03 02:58:34 +0200

    Adding a current year in the footer text

--- app/(private)/footer.tsx
+++ app/(private)/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
     <footer className="container md:w-full flex justify-between items-center text-xs opacity-50 mt-3">
       <small className="text-xs">
         <p>
-          © Copyright by{" "}
+          {currentDate.getFullYear()} © Copyright by{" "}
           <Link
             className=""
             href="https://s-antonov-portfolio-website.vercel.app/"
