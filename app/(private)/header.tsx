18c4b7bbf93fb8a2d64c5ec86dcf43beed12466c
Antonov-S <anto1nov1@gmail.com>
2025-01-05 04:14:19 +0200

    Refactor swr config in private layout

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -2,15 +2,11 @@
 
 import useSWR from "swr";
 
-import fetcher from "../util/fetcher";
-
 export default function Header() {
-  const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+  const { data, error, isLoading } = useSWR("/api/users/profile");
 
   if (error) return <div>Faild to load</div>;
   if (isLoading) return <div>Loading...</div>;
 
-  console.log(data);
-
-  return <header>Header</header>;
+  return <header>{data.data.username}</header>;
 }
--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -1,16 +1,22 @@
+"use client";
+
 import { ReactNode } from "react";
+import { SWRConfig } from "swr";
 
 import Header from "./header";
 import NavBar from "./navbar";
 import Footer from "./footer";
+import fetcher from "../util/fetcher";
 
 export default function PrivateLayout({ children }: { children: ReactNode }) {
   return (
-    <div>
-      <Header />
-      <NavBar />
-      {children}
-      <Footer />
-    </div>
+    <SWRConfig value={{ fetcher: fetcher }}>
+      <div>
+        <Header />
+        <NavBar />
+        {children}
+        <Footer />
+      </div>
+    </SWRConfig>
   );
 }
