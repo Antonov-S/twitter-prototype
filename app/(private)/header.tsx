116dc99adb5ab975ff2a4332be0f610dd6750192
Antonov-S <anto1nov1@gmail.com>
2025-01-05 04:02:51 +0200

    Swr and fetcher

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import useSWR from "swr";
+
+import fetcher from "../util/fetcher";
+
 export default function Header() {
+  const { data, error, isLoading } = useSWR("/api/users/profile", fetcher);
+
+  if (error) return <div>Faild to load</div>;
+  if (isLoading) return <div>Loading...</div>;
+
+  console.log(data);
+
   return <header>Header</header>;
 }
