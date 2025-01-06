b7c9394c3faa2289ff2123f5622cc7f85dbb44cb
Antonov-S <anto1nov1@gmail.com>
2025-01-06 02:35:55 +0200

    Highlight active link

--- app/(private)/navbar.tsx
+++ app/(private)/navbar.tsx
@@ -1,20 +1,58 @@
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 export default function NavBar() {
+  const pathname = usePathname();
   return (
     <nav className="flex max-w-md w-full p-5 bg-slate-800 rounded-lg">
       <ul className="flex flex-row justify-around w-full">
         <li>
-          <Link href="/feed">Feed</Link>
+          <Link
+            href="/feed"
+            className={
+              pathname.startsWith("/feed")
+                ? "dark:text-green-400 text-green-800"
+                : ""
+            }
+          >
+            Feed
+          </Link>
         </li>
         <li>
-          <Link href="/profile">Profile</Link>
+          <Link
+            href="/profile"
+            className={
+              pathname.startsWith("/profile")
+                ? "dark:text-green-400 text-green-800"
+                : ""
+            }
+          >
+            Profile
+          </Link>
         </li>
         <li>
-          <Link href="/following">Following</Link>
+          <Link
+            href="/following"
+            className={
+              pathname.startsWith("/following")
+                ? "dark:text-green-400 text-green-800"
+                : ""
+            }
+          >
+            Following
+          </Link>
         </li>
         <li>
-          <Link href="/followers">Followers</Link>
+          <Link
+            href="/followers"
+            className={
+              pathname.startsWith("/followers")
+                ? "dark:text-green-400 text-green-800"
+                : ""
+            }
+          >
+            Followers
+          </Link>
         </li>
       </ul>
     </nav>
