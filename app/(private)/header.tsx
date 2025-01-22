1ab8845a570ec1d67b982412e91d58460fa38064
Antonov-S <anto1nov1@gmail.com>
2025-01-22 02:46:30 +0200

    Adding Account page

--- app/(private)/header.tsx
+++ app/(private)/header.tsx
@@ -15,7 +15,7 @@ export default function Header() {
         <h1 className="font-mono text-lg">Strings</h1>
       </div>
       <div>
-        <User user={data.data} href="/account" />
+        <User user={data.data} href="account" />
       </div>
     </header>
   );
