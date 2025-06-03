2afd8d43026d37ba6a43662643be0de05b217823
Antonov-S <anto1nov1@gmail.com>
2025-06-03 16:49:03 +0300

    Adding get users endpoint

---
+++ app/api/users/route.ts
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+
+import { sql } from "@/db";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const username = searchParams.get("username");
+
+  if (!username) {
+    return NextResponse.json(
+      { error: "username filter required" },
+      { status: 400 }
+    );
+  }
+
+  const statement =
+    "select id, username, avatar from users where username ilike $1";
+  const values = [username];
+  const res = await sql(statement, values);
+
+  return NextResponse.json({ data: res.rows });
+}
