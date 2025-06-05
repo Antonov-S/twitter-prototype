36a668df9a9f8a4544b4d1d6d28035c75b038eaf
Antonov-S <anto1nov1@gmail.com>
2025-06-06 01:42:05 +0300

    Install lodash, debounce search

--- app/(private)/layout.tsx
+++ app/(private)/layout.tsx
@@ -7,11 +7,13 @@ import Header from "./header";
 import NavBar from "./navbar";
 import Footer from "./footer";
 import fetcher from "../util/fetcher";
+import SearchBar from "./search-bar";
 
 export default function PrivateLayout({ children }: { children: ReactNode }) {
   return (
     <SWRConfig value={{ fetcher: fetcher }}>
       <div className="flex flex-col min-h-screen max-w-md m-auto items-center justify-center">
+        <SearchBar />
         <Header />
         <NavBar />
         <main className="w-full p-5 bg-slate-800 rounded-lg my-2">
---
+++ app/(private)/search-bar.tsx
@@ -0,0 +1,36 @@
+"use client";
+import * as _ from "lodash";
+import { ChangeEvent, useState } from "react";
+
+export default function SearchBar() {
+  const [searchResults, setSearchResults] = useState([]);
+
+  const debouncedFetchSearchResults = _.debounce(fetchSearchResults, 500);
+
+  async function fetchSearchResults(searchText: string) {
+    const res = await fetch("/api/search?q=" + searchText);
+
+    if (res.ok) {
+      const json = await res.json();
+      console.log(json);
+
+      setSearchResults(json.data);
+    }
+  }
+
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    console.log("change", e.target.value);
+    debouncedFetchSearchResults(e.target.value);
+  }
+
+  return (
+    <div>
+      <input
+        onChange={handleChange}
+        type="text"
+        className="p-2 rounded-lg bg-gray-700 my-2"
+        placeholder="Search"
+      />
+    </div>
+  );
+}
